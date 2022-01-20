b596faf3d923dbd3969939bfd2ffb3879036c221
Munsi Abdul Al Mamun <mdabdulalmamoon@gmail.com>
2022-01-20 14:40:41 +0800

    Add project files.

---
+++ StackApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] myArray = new int[] { 1, 2, 3, 4};
+            Stack<string> myStack = new Stack<string>();
+            myStack.Push("A");
+            myStack.Push("B");
+            myStack.Push("C");
+            myStack.Push("D");
+
+            
+           // Console.WriteLine();
+
+
+            foreach (var item in myStack)
+            {
+                Console.WriteLine(item + ",");
+            }
+
+            string popName = myStack.Pop();
+
+            Console.WriteLine("------------------");
+            Console.WriteLine(popName);
+
+            //myStack.Pop();
+
+            Console.WriteLine("------------------");
+            string name = myStack.Peek();
+            Console.WriteLine(name);
+            Console.WriteLine("------------------");
+
+            foreach (var item in myStack)
+            {
+                Console.WriteLine(item + ",");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
